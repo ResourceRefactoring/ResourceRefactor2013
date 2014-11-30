0850e6cc38baacd82838c9e8d0c54a7ed57e817a
obrestad <jan@obrestad.info>
2014-11-30 18:03:26 +0100

    Initial state from wizard

---
+++ ResourceRefactor2013/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace ResourceRefactoring.ResourceRefactor2013
+{
+    static class GuidList
+    {
+        public const string guidResourceRefactor2013PkgString = "1bd39d38-1eb0-423f-bc40-46301c752ace";
+        public const string guidResourceRefactor2013CmdSetString = "ab43283b-3fe3-43d8-bb35-b2758157469c";
+
+        public static readonly Guid guidResourceRefactor2013CmdSet = new Guid(guidResourceRefactor2013CmdSetString);
+    };
+}
---
+++ ResourceRefactor2013/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace ResourceRefactoring.ResourceRefactor2013
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidExtractResource =        0x100;
+
+
+    };
+}
